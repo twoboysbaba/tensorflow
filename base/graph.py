4ec40a6f7f757387e715bf243582488424516a17
zhaolei.06 <zhaolei.06@bytedance.com>
2022-05-27 15:39:32 +0800

    add

---
+++ base/graph.py
@@ -0,0 +1,47 @@
+#计算图 20220527
+
+import tensorflow as tf
+tf.compat.v1.disable_eager_execution()
+# 创建一个常量 op, 产生一个 1x2 矩阵. 这个 op 被作为一个节点
+# 加到默认图中.
+# 构造器的返回值代表该常量 op 的返回值.
+matrix1 = tf.constant([[3., 3.]])
+
+# 创建另外一个常量 op, 产生一个 2x1 矩阵.
+matrix2 = tf.constant([[2.],[2.]])
+
+# 创建一个矩阵乘法 matmul op , 把 'matrix1' 和 'matrix2' 作为输入.
+# 返回值 'product' 代表矩阵乘法的结果.
+product = tf.matmul(matrix1, matrix2)
+
+# 启动默认图.
+sess = tf.compat.v1.Session()
+
+# 调用 sess 的 'run()' 方法来执行矩阵乘法 op, 传入 'product' 作为该方法的参数. 
+# 上面提到, 'product' 代表了矩阵乘法 op 的输出, 传入它是向方法表明, 我们希望取回
+# 矩阵乘法 op 的输出.
+# 整个执行过程是自动化的, 会话负责传递 op 所需的全部输入. op 通常是并发执行的.
+# 函数调用 'run(product)' 触发了图中三个 op (两个常量 op 和一个矩阵乘法 op) 的执行.
+# 返回值 'result' 是一个 numpy `ndarray` 对象.
+result = sess.run(product)
+print(result)
+# ==> [[ 12.]]
+# 任务完成, 关闭会话.
+sess.close()
+
+#对象在使用完后需要关闭以释放资源. 除了显式调用 close 外, 也可以使用 "with" 代码块 来自动完成关闭动作.
+with tf.compat.v1.Session() as sess:
+    result = sess.run([product])
+    print(result)
+    #==》[array([[12.]], dtype=float32)]
+
+# 进入一个交互式 TensorFlow 会话.
+sess = tf.compat.v1.InteractiveSession()
+x = tf.Variable([1.0, 2.0])
+a = tf.constant([3.0, 3.0])
+# 使用初始化器 initializer op 的 run() 方法初始化 'x' 
+x.initializer.run()
+# 增加一个减法 sub op, 从 'x' 减去 'a'. 运行减法 op, 输出结果 
+sub = tf.subtract(x, a)
+print(sub.eval())
+# ==> [-2. -1.]
